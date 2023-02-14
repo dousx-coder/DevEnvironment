3b4f0f7770fda3850279f4c401a538b8f0bfb54b
dousx <dousx_cn@163.com>
2023-02-14 22:50:08 +0800

    vmIp

--- Docker/Docker常用软件安装/Redis/sentinel-slaveof/config.sh
+++ Docker/Docker常用软件安装/Redis/sentinel-slaveof/config.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 rm /data/redis/ -rf
+
+vmIp=192.168.96.151
+
 echo "create redis conf..."
 for port in $(seq 1 3);
 do
@@ -8,12 +11,13 @@ touch /data/redis/node-${port}/conf/redis.conf
 cat << EOF >/data/redis/node-${port}/conf/redis.conf
 port 6379
 bind 0.0.0.0
-replica-announce-ip 172.20.0.1${port}
-slaveof 172.20.0.11 6379
+slaveof ${vmIp} 6371
 appendonly no
+slave-announce-ip ${vmIp}
+slave-announce-port 637${port}
 EOF
 done
-sed -i 's/slaveof 172.20.0.11 6379//g' /data/redis/node-1/conf/redis.conf
+sed -i "s/slaveof ${vmIp} 6371//g" /data/redis/node-1/conf/redis.conf
 
 # --------------------------------------------------------------------
 
@@ -25,11 +29,11 @@ mkdir -p /data/redis/sentinel-${port}/conf
 touch /data/redis/sentinel-${port}/conf/sentinel.conf
 cat << EOF >/data/redis/sentinel-${port}/conf/sentinel.conf
 port 26379
-sentinel monitor redis-nodes 172.20.0.11 6379 2
+sentinel monitor redis-nodes ${vmIp} 6371 2
 protected-mode no
 sentinel down-after-milliseconds redis-nodes 30000
 sentinel deny-scripts-reconfig yes
-sentinel announce-ip 192.168.96.151
+sentinel announce-ip ${vmIp}
 sentinel announce-port 2637${port}
 EOF
 done
